7e8498e12bef6ad339a8bad23f020bd6353fdecf
aszulist <aszulist@o2.pl>
2018-03-17 22:27:00 +0100

    Fix assets and add video player js

---
+++ app/Resources/js/video-player.js
@@ -0,0 +1,39 @@
+var VideoPlayer = {
+
+    videoTag: null,
+    afterVideoRedirectPath: null,
+
+    init: function () {
+        this.videoTag = $("#doomVideo");
+        this.afterVideoRedirectPath = $("#afterVideoPath");
+
+        if (this.videoTag.length) {
+            this.clickListener();
+        }
+
+        if (this.afterVideoRedirectPath.length) {
+            this.endListener();
+        }
+    },
+
+    endListener: function () {
+        var $this = this;
+        $this.videoTag.on('ended', function() {
+            window.location.href = $this.afterVideoRedirectPath.attr('href');
+        })
+    },
+
+    clickListener: function () {
+        this.videoTag.on('click', function () {
+            var video = $(this).get(0);
+
+            if (video.paused === false) {
+                video.pause();
+            } else {
+                video.play();
+            }
+        });
+    }
+};
+
+VideoPlayer.init();
